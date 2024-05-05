e51129548b17b1c4080dd56e85a4572ecb40a763
Stella Ullman <stellarainullman@gmail.com>
2024-05-05 17:03:28 -0400

    Greedy Test File Added

---
+++ Test_Greedy.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+
+// public class Test_Greedy{
+//     //instance of Greedy
+//     //instance of Backtracking
+
+//     for(int i = 0; i <= 10; i++){
+//         //instance of graph  
+//         //test runtime for 10^i vertices
+//     }
+
+//     //special cases:
+//     //no path -- 3+ nodes with one edge, 1+ node with no edge
+//     //confuse greedy algorithm?
+
+// }
